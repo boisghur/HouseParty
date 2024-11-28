8481fc242682f494a5107e0967345edaed088615
boisghur <74437002+boisghur@users.noreply.github.com>
2024-11-28 08:03:40 +0400

    Room Settings After Creation
    * Allows room host to alter room settings after creation
    * New API endpoint to allow room updates in backend

--- website/frontend/src/components/Room.js
+++ website/frontend/src/components/Room.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { Button } from "@material-ui/core";
-
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import CreateRoomPage from "./CreateRoomPage";
 function Room() {
   const [votesToSkip, setVotesToSkip] = useState(2);
   const [guestCanPause, setGuestCanPause] = useState(true);
   const [isHost, setIsHost] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
   const { roomCode } = useParams();
   const navigate = useNavigate();
 
@@ -21,7 +24,7 @@ function Room() {
 
   const LeaveRoomButton = () => {
     const requestOptions = {
-      method: "POST",
+      method: "DELETE",
       headers: { "Content-Type": "application/json" },
     };
     fetch("/api/leave-room", requestOptions)
@@ -30,16 +33,80 @@ function Room() {
       .catch((error) => console.error("Error:", error));
   };
 
+  function renderSettings() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <CreateRoomPage
+            update={true}
+            votesToSkip={votesToSkip}
+            guestCanPause={guestCanPause}
+            roomCode={roomCode}
+            updateCallback={null}
+          />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => setShowSettings(false)}
+          >
+            Close
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  function renderSettingsButton() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    );
+  }
+
+  if (showSettings) {
+    return renderSettings();
+  }
   return (
-    <div>
-      <h3>{roomCode}</h3>
-      <p> Votes: {votesToSkip}</p>
-      <p> Pause: {guestCanPause.toString()}</p>
-      <p> Host: {isHost.toString()}</p>
-      <Button color="secondary" variant="outlined" onClick={LeaveRoomButton}>
-        Exit Room
-      </Button>
-    </div>
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography component="h2" variant="h2">
+          {roomCode}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography component="h6" variant="h6">
+          {" "}
+          Votes: {votesToSkip}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography component="h6" variant="h6">
+          {" "}
+          Pause: {guestCanPause.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography component="h6" variant="h6">
+          {" "}
+          Host: {isHost.toString()}
+        </Typography>
+      </Grid>
+      {isHost ? renderSettingsButton() : null}
+      <Grid item xs={12} align="center">
+        <Button color="secondary" variant="outlined" onClick={LeaveRoomButton}>
+          Exit Room
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
