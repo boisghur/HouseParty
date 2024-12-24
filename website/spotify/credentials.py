8fb4c9be9edd1e7acee64ff2bd57ee7482418acd
boisghur <74437002+boisghur@users.noreply.github.com>
2024-12-24 20:06:15 +0400

    fix redirect

--- website/spotify/credentials.py
+++ website/spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "cb42d0df51ab48b2b9a9dde7b3817cd1"
 CLIENT_SECRET = "0a82b6ca78084f348a1cf1e59bebad62"
-REDIRECT_URI = "https://houseparty.onrender.com/"
+REDIRECT_URI = "https://houseparty.onrender.com/spotify/redirect"
